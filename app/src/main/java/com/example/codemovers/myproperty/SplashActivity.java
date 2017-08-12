cc952ecf3b0758c44e24fa5c00ba1bea93cc874a
Muyinda Rogers <moverr@gmail.com>
2017-08-12 11:10:30 +0300

    PRO_Layout Design Basic Splash and Login Screen, not fully complete

--- app/src/main/java/com/example/codemovers/myproperty/SplashActivity.java
+++ app/src/main/java/com/example/codemovers/myproperty/SplashActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 
 
@@ -69,6 +70,10 @@ public class SplashActivity extends AppCompatActivity {
         m_bpaused = false;
     }
 
-    
-
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+         super.onKeyDown(keyCode, event);
+        m_bSplashActive = false;
+        return  true;
+    }
 }
