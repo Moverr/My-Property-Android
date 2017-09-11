e259a0f0c19cf8ffadfe428667606b8b7bd94e0e
Muyinda Rogers <moverr@gmail.com>
2017-09-11 22:08:33 +0300

    PRO_MOBILE DAO authentication

---
+++ app/src/main/java/myproperty/codemovers/myproperty/dao/AuthenticationDAO.java
@@ -0,0 +1,41 @@
+package myproperty.codemovers.myproperty.dao;
+
+import java.util.List;
+
+/**
+ * Created by Manny on 9/11/2017.
+ */
+
+public class AuthenticationDAO {
+
+    private String authorization;
+    private List<PermissionResponse> permissions;
+    private Boolean isLoggedIn;
+
+    public AuthenticationDAO() {
+    }
+
+    public String getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(String authorization) {
+        this.authorization = authorization;
+    }
+
+    public List<PermissionResponse> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<PermissionResponse> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Boolean getLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(Boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+}
---
+++ app/src/main/java/myproperty/codemovers/myproperty/dao/PermissionResponse.java
@@ -0,0 +1,72 @@
+package myproperty.codemovers.myproperty.dao;
+
+/**
+ * Created by Manny on 9/11/2017.
+ */
+
+class PermissionResponse {
+    private Integer id;
+    private String grouping;
+    private String code;
+    private String status;
+    private String name;
+
+    public PermissionResponse(){
+    }
+
+    public PermissionResponse(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGrouping() {
+        return grouping;
+    }
+
+    public void setGrouping(String grouping) {
+        this.grouping = grouping;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getCanonicalName()
+                +"{"
+                +"id="+id
+                +"grouping="+grouping
+                +"code="+code
+                +"status="+status
+                +"name="+name
+                +"}";
+    }
+}
