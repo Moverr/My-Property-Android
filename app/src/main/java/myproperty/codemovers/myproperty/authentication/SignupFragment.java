134114b94a1cec3e4eae0fef8160c910270f299f
Muyinda Rogers <moverr@gmail.com>
2017-09-16 14:23:02 +0300

    PRO_MOBILE INitializing Registration

--- app/src/main/java/myproperty/codemovers/myproperty/authentication/SignupFragment.java
+++ app/src/main/java/myproperty/codemovers/myproperty/authentication/SignupFragment.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import myproperty.codemovers.myproperty.R;
 import myproperty.codemovers.myproperty.core.BaseFragment;
@@ -16,6 +19,10 @@ import myproperty.codemovers.myproperty.core.BaseFragment;
 public class SignupFragment  extends BaseFragment {
 
     private static SignupFragment instance = null;
+
+    EditText names,email_address,password,repassword;
+
+    Button register;
 
     public static SignupFragment getInstance(){
         if(instance == null){
@@ -34,7 +41,26 @@ public class SignupFragment  extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.signup_screen,container,false);
+
+        names = (EditText) view.findViewById(R.id.names);
+        email_address = (EditText) view.findViewById(R.id.email_address);
+        password = (EditText) view.findViewById(R.id.password);
+        repassword = (EditText) view.findViewById(R.id.repassword);
+        register = (Button) view.findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleRegister();
+            }
+        });
+
         return view;
 
+    }
+
+    // plates are done
+    private void handleRegister() {
+        Toast.makeText(getContext(), "Handler Registration ", Toast.LENGTH_SHORT).show();
     }
 }
